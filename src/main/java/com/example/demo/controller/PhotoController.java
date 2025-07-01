3b672e0e0c6e8176fc4116f0c666dd214ecb14be
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2025-07-01 12:09:13 +0800

    Updated PhotoController.java
    Added a DELETE mapping to /photo/{id} and a POST mapping to /photo

--- src/main/java/com/example/demo/controller/PhotoController.java
+++ src/main/java/com/example/demo/controller/PhotoController.java
@@ -2,28 +2,46 @@ package com.example.demo.controller;
 
 import com.example.demo.model.PhotoModel;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PhotoController {
-  private List<PhotoModel> db = List.of(new PhotoModel("1", "Photo1"),
-                                         new PhotoModel("2", "Photo2"),
-                                         new PhotoModel("3", "Photo3"));
+  private final Map<String, PhotoModel> db = new HashMap<>() {{
+    put("1", new PhotoModel("1", "photo.jpeg"));
+    put("2", new PhotoModel("2", "photo2.jpeg"));
+  }};
 
   @GetMapping("/photo")
-  private List<PhotoModel> getPhoto() {
-    return db;
+  public Collection<PhotoModel> getPhoto() {
+    return db.values();
+  }
+
+  @PostMapping("/photo")
+  public PhotoModel create(@RequestBody PhotoModel photoModel) {
+    photoModel.setId(UUID.randomUUID().toString());
+    db.put(photoModel.getId(), photoModel);
+    return photoModel;
   }
 
   @GetMapping("/photo/{id}")
   private PhotoModel getPhotoById(@PathVariable String id) {
-    return db.stream()
-             .filter(photo -> photo.getId().equals(id))
-             .findFirst()
-             .orElse(null);
+    PhotoModel photoGetByID = db.get(id);
+    if (photoGetByID == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo not found");
+    }
+    return photoGetByID;
+  }
+
+  @DeleteMapping("/photo/{id}")
+  public void deletePhoto(@PathVariable String id) {
+    PhotoModel photoDeleteByID = db.remove(id);
+    if (photoDeleteByID == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
   }
 }
